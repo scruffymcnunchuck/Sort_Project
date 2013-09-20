42d8945b8afe9c071ff9f83f98e4c6b18aaf2f67
dsarge101 <sargejr@dhcp-10-201-112-252.tamulink.tamu.edu>
2013-09-20 15:09:22 -0500

    Radix Extra Functions
    Added three functions in order to determine the digit at each position

--- radix-sort.cpp
+++ radix-sort.cpp
@@ -9,13 +9,45 @@
 #include "sort.h"
 #include <iostream>
 
+int power(int base, int power){
+    if ( power == 0 ) { return 1; }
+    else {
+        int value = base;
+        for ( int i = 1; i < two; ++i ) {
+            value = value * base;
+        }
+        return value;
+    }
+}
+
+int mod_num(int n, int exponent){
+    if ( exponent > 1 ) {
+        return n % power(10,exponent) - n % power(10,exponent-1);
+    }
+    if ( exponent == 1 ) { return n % 10; }
+}
+
+int determine_size(int n){
+    int s;
+    while ( n > 0 ){
+        n=n/10;
+        ++s;
+    }
+    return s;
+}
+
 void
 RadixSort::sort(int A[], int size)
 {
+    
+    for ( int i = 0; i < 5; ++i ){
+        int exp = determine_size(test_array[i]);
+        cout << mod_num(A[i]);
+    }
+    
   /*
      Complete this function with the implementation of radix sort
      algorithm.
   */
   
-  cout << "stuff";
 }
